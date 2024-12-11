e6abfb95ec1d9fb2afc0124b6a6ca7e4420a7480
Diog007 <diogo.mz123@gmail.com>
2024-12-11 17:42:03 -0300

    project initial

---
+++ src/test/java/com/eventosnet/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosnet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
