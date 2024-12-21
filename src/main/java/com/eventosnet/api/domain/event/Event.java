f9d7c6ea6c9bea7b89781fc84960b6d2c6c804dc
Diog007 <diogo.mz123@gmail.com>
2024-12-21 20:59:45 -0300

    taken notes from lombok

--- src/main/java/com/eventosnet/api/domain/event/Event.java
+++ src/main/java/com/eventosnet/api/domain/event/Event.java
@@ -1,20 +1,11 @@
 package com.eventosnet.api.domain.event;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.Date;
 import java.util.UUID;
 
 @Table(name = "event")
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Event {
 
     @Id
@@ -30,4 +21,73 @@ public class Event {
     @Lob
     private byte[] image;
 
+    public Event() {
+    }
+
+    public Event(UUID id, String title, String description, String eventUrl, Boolean remote, Date date, byte[] image) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.eventUrl = eventUrl;
+        this.remote = remote;
+        this.date = date;
+        this.image = image;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    public Boolean getRemote() {
+        return remote;
+    }
+
+    public void setRemote(Boolean remote) {
+        this.remote = remote;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }
