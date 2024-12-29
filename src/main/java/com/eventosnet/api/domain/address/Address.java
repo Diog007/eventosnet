241e19f5569ced0a16b8ecab216b852131e56e29
Diog007 <diogo.mz123@gmail.com>
2024-12-29 10:34:06 -0300

    Adjusted Address class

--- src/main/java/com/eventosnet/api/domain/address/Address.java
+++ src/main/java/com/eventosnet/api/domain/address/Address.java
@@ -1,21 +1,11 @@
 package com.eventosnet.api.domain.address;
 
-
 import com.eventosnet.api.domain.event.Event;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.UUID;
 
 @Entity
 @Table(name = "address")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Address {
 
     @Id
@@ -30,4 +20,35 @@ public class Address {
     @JoinColumn(name = "event_id")
     private Event event;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
 }
