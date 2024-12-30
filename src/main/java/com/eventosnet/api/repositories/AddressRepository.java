d1cb2adc1cddddd3cc8c58ece9f08a909fc945e0
Diog007 <diogo.mz123@gmail.com>
2024-12-30 17:50:07 -0300

    Search method implemented Jpa

--- src/main/java/com/eventosnet/api/repositories/AddressRepository.java
+++ src/main/java/com/eventosnet/api/repositories/AddressRepository.java
@@ -1,10 +1,12 @@
 package com.eventosnet.api.repositories;
 
 import com.eventosnet.api.domain.address.Address;
-import com.eventosnet.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AddressRepository extends JpaRepository<Address, UUID> {
+
+    public Optional<Address> findByEventId(UUID eventId);
 }
